dd85a1f250e056cad394a3dc0b88bb6ac85ff844
seongseonko <kymgok1001@naver.com>
2024-06-28 10:08:22 +0900

    test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,17 @@
-describe('template spec', () => {
+describe('dev', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+    cy.contains('p', 'Get started').should('include.text', 'Get started');
+  });
+
+  it('a태그 링크 타고 넘어가기', () => {
+    cy.get('a')
+      .should((a) => {
+        expect(a.attr('target', 'target')).to.equal('_blank');
+        a.attr('target', '_self');
+      })
+      .click();
+  });
+});
