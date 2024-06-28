107fe695ffd8bc033296c3bb2fa7f3c2fcc7a802
seongseonko <kymgok1001@naver.com>
2024-06-28 11:06:41 +0900

    cypress

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,17 +1,13 @@
-describe('dev', () => {
+describe('template spec', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000');
   });
-  it('passes', () => {
-    cy.contains('p', 'Get started').should('include.text', 'Get started');
+
+  it('should contain "Get started by ~" text', () => {
+    cy.contains('Get started by editing');
   });
 
-  it('a태그 링크 타고 넘어가기', () => {
-    cy.get('a')
-      .should((a) => {
-        expect(a.attr('target', 'target')).to.equal('_blank');
-        a.attr('target', '_self');
-      })
-      .click();
+  it('should contain "Docs" heading anchor with correct href prop', () => {
+    cy.contains('Docs').should('have.attr', 'href').and('include', 'https://nextjs.org/docs');
   });
 });
