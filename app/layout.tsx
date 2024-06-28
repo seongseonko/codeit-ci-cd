729acd62c742f43420a7049fcf6fbf36ede85dc8
seongseonko <kymgok1001@naver.com>
2024-06-28 11:05:37 +0900

    speedInsight

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-      <SpeedInsights />
-    </html>
+    <>
+      <html lang='en'>
+        <body className={inter.className}>
+          {children}
+          <SpeedInsights />
+        </body>
+      </html>
+    </>
   );
 }
